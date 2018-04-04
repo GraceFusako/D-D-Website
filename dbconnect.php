b172738ed3f10c84b103ad4dbf438af0ac08bd42
LucasGI <jgolsen1203@gmail.com>
2018-04-04 14:16:18 -0600

    4/4/2018 2:15PM
    Added PHP for user sessions.

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+//set up user login/logout sessions, used for future files
+$db_host = "localhost";
+$db_name = "db";
+$db_user = "root";
+$db_pass = "toor";
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
